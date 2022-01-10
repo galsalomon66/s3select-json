070a3f203319d2f7d799119518d9a3bd357078a3
gal salomon <gal.salomon@gmail.com>
2022-01-10 08:19:15 +0200

    adding comments for sax parser
    Signed-off-by: gal salomon <gal.salomon@gmail.com>

--- json_parser.cpp
+++ json_parser.cpp
@@ -18,15 +18,15 @@
 using namespace rapidjson;
 
 class Valuesax {
-public:
+  public:
     enum Type {
-        Decimal,
-        Double,
-        String,
-        Bool,
-        Null
+      Decimal,
+      Double,
+      String,
+      Bool,
+      Null
     };
-    
+
     static Valuesax Parse(std::string const& s);
     static Valuesax Parse(const double& s);
     static Valuesax Parse(bool& s);
@@ -38,26 +38,26 @@ public:
     Type type() const { return _type; }
 
     int asInt() const {
-        assert(_type == Decimal && "not an int");
-        return _num;
+      assert(_type == Decimal && "not an int");
+      return _num;
     }
 
     double asDouble() const {
-        assert(_type == Double && "not a double");
-        return _double;
+      assert(_type == Double && "not a double");
+      return _double;
     }
 
     std::string const& asString() const {
-        assert(_type == String && "not a string");
-        return _string;
+      assert(_type == String && "not a string");
+      return _string;
     }
 
     bool asBool() const {
-        assert(_type == Bool && "not a bool");
-        return _bool;
+      assert(_type == Bool && "not a bool");
+      return _bool;
     }
 
-private:
+  private:
     Type _type;
     double _double;
     std::string _string;
@@ -67,47 +67,47 @@ private:
 };
 
 Valuesax Valuesax::Parse(std::string const& s) {
-    Valuesax result;
-    result._type = Valuesax::String;
+  Valuesax result;
+  result._type = Valuesax::String;
 
-    result._string = s;
-    return result;
+  result._string = s;
+  return result;
 }
 
 Valuesax Valuesax::Parse(const double& s) {
-    Valuesax result;
-    result._type = Valuesax::Double;
+  Valuesax result;
+  result._type = Valuesax::Double;
 
-    result._double = s;
-    return result;
+  result._double = s;
+  return result;
 }
 
 Valuesax Valuesax::Parse(bool& s) {
-    Valuesax result;
-    result._type = Valuesax::Bool;
+  Valuesax result;
+  result._type = Valuesax::Bool;
 
-    result._bool = s;
-    return result;
+  result._bool = s;
+  return result;
 }
 
 Valuesax Valuesax::Parse(const unsigned& s) {
-    Valuesax result;
-    result._type = Valuesax::Decimal;
+  Valuesax result;
+  result._type = Valuesax::Decimal;
 
-    result._num = s;
-    return result;
+  result._num = s;
+  return result;
 }
 
 Valuesax Valuesax::Parse(const std::nullptr_t& s) {
-    Valuesax result;
-    result._type = Valuesax::Null;
+  Valuesax result;
+  result._type = Valuesax::Null;
 
-    result._null = s;
-    return result;
+  result._null = s;
+  return result;
 }
 
 class MyHandler : public BaseReaderHandler<UTF8<>, MyHandler> {
-    public:
+  public:
     std::vector < std::pair < std::string, Valuesax>>  mymap;
     std::vector <char> vect;
     std::string keyname{"first"};
@@ -115,100 +115,108 @@ class MyHandler : public BaseReaderHandler<UTF8<>, MyHandler> {
     bool valuep{false};
     int start_counter{0};
     std::vector<std::string> mystack;
-    std::unordered_map<int, std::string> key_stack;
+    std::unordered_map<int, std::string> key_stack;//TODO why not using an array?
+
     bool Null() {
       mymap.push_back(std::make_pair(keyvalue, Valuesax::Parse(nullptr)));
       valuep = true; 
       return true; }
+
     bool Bool(bool b) {
       mymap.push_back(std::make_pair(keyvalue, Valuesax::Parse(b)));
       valuep = true;
       return true; }
-    bool Int(int i) { 
+
+    bool Int(int i) {//TODO to complete 
       return true; }
+
     bool Uint(unsigned u) {
       mymap.push_back(std::make_pair(keyvalue, Valuesax::Parse(u)));
       valuep = true;
       return true; }
-    bool Int64(int64_t i) { 
+
+    bool Int64(int64_t i) {//TODO to complete  
       return true; }
-    bool Uint64(uint64_t u) { 
+
+    bool Uint64(uint64_t u) {//TODO to complete  
       return true; }
+
     bool Double(double d) { 
       mymap.push_back(std::make_pair(keyvalue, Valuesax::Parse(d)));
       valuep = true;
       return true; }
+
     bool String(const char* str, SizeType length, bool copy) {
-        mymap.push_back(std::make_pair(keyvalue, Valuesax::Parse(str)));
-        valuep = true;
-        return true;
-    }
-    
-  bool StartObject() {
-    if(!valuep) {
-      if (mystack.size() == 0 || mystack[mystack.size() - 1] != keyname) {
-        if (keyname != "first") {
-        mystack.push_back(keyname);
-        start_counter = vect.size();
-        key_stack[start_counter] = keyname;
-        }
-      }
-    }
-    vect.push_back('{');
-    return true; 
+      mymap.push_back(std::make_pair(keyvalue, Valuesax::Parse(str)));
+      valuep = true;
+      return true;
     }
 
-  bool Key(const char* str, SizeType length, bool copy) {
-    std::vector<std::string> stack{mystack};
-    stack.push_back(str);
-    valuep = false;
-    keyvalue = "";
-    
-    for (const auto& i: stack) {
-      keyvalue += i + '/';
-    }
-    keyname = str;
-      
-    return true;
+    bool StartObject() {
+      if(!valuep) {
+	if (mystack.size() == 0 || mystack[mystack.size() - 1] != keyname) {
+	  if (keyname != "first") {//TODO "first" could be real key-name, it could not be used as a state
+	    mystack.push_back(keyname);
+	    start_counter = vect.size();
+	    key_stack[start_counter] = keyname;
+	  }
+	}
+      }
+      vect.push_back('{');
+      return true; 
     }
 
-  bool EndObject(SizeType memberCount) {
-    vect.pop_back();
-    if (mystack.size() > 0 && vect.size()  == start_counter) {
-      if(key_stack[vect.size()] == mystack.back()) {
-        mystack.pop_back();
+    bool Key(const char* str, SizeType length, bool copy) {
+      std::vector<std::string> stack{mystack}; //TODO on each call it construct stack with mystack
+      stack.push_back(str);
+      valuep = false;
+      keyvalue = "";
+
+      for (const auto& i: stack) {
+	keyvalue += i + '/';
       }
-      --start_counter;
-    }
-    return true;
+      keyname = str;
+
+      return true;
     }
 
-  bool StartArray() {
-    if(!valuep) {
-      if (mystack.size() == 0 || mystack[mystack.size() - 1] != keyname) {
-        mystack.push_back(keyname);
-        start_counter = vect.size();
-        key_stack[start_counter] = keyname;
+    bool EndObject(SizeType memberCount) {
+      vect.pop_back();
+      if (mystack.size() > 0 && vect.size()  == start_counter) {
+	if(key_stack[vect.size()] == mystack.back()) {
+	  mystack.pop_back();
+	}
+	--start_counter;
       }
+      return true;
     }
-    vect.push_back('[');
-    return true; 
-  }
 
-  bool EndArray(SizeType elementCount) { 
-    vect.pop_back();
-    if (mystack.size() > 0 && vect.size()  == start_counter) {
-      if(key_stack[vect.size()] == mystack.back()) {
-        mystack.pop_back();
+    bool StartArray() {
+      if(!valuep) {
+	if (mystack.size() == 0 || mystack[mystack.size() - 1] != keyname) {
+	  mystack.push_back(keyname);
+	  start_counter = vect.size();
+	  key_stack[start_counter] = keyname;
+	}
       }
-    --start_counter;
-  }
-    return true; 
-  }
+      vect.push_back('[');
+      return true; 
+    }
+
+    bool EndArray(SizeType elementCount) { 
+      vect.pop_back();
+      if (mystack.size() > 0 && vect.size()  == start_counter) {
+	if(key_stack[vect.size()] == mystack.back()) {
+	  mystack.pop_back();
+	}
+	--start_counter;
+      }
+      return true; 
+    }
 };
 
 std::string parse_json_sax(const char* file_name)
-{
+{// the parser is actually load the whole input (several GB .... )
   std::string stringFromStream;
   std::ifstream in;
   std::stringstream result;
@@ -218,7 +226,7 @@ std::string parse_json_sax(const char* file_name)
   if (in.is_open()) {
     std::string line;
     while (getline(in, line)) {
-        stringFromStream.append(line + "\n");
+      stringFromStream.append(line + "\n"); //the entire file is loaded into stringFromStream
     }
     in.close();
   }
@@ -230,7 +238,7 @@ std::string parse_json_sax(const char* file_name)
   StringStream ss(data);
   reader.Parse(ss, handler);
 
-  for (auto const& i: handler.mymap) {
+  for (auto const& i: handler.mymap) { //mymap contains all keys/vause, could be huge amount of data
     result<<i.first<<": ";
     switch((i.second).type()) {
       case Valuesax::Decimal: result << (i.second).asInt() << "\n"; break;
@@ -271,22 +279,22 @@ void dom_traverse_v2::print(const Value &v, std::string key_name)
     else
       if(v.IsBool())
       {
-  ss << (v.GetBool() ? "true" : "false" ) << std::endl;
+	ss << (v.GetBool() ? "true" : "false" ) << std::endl;
       }
       else
-  if(v.IsNull())
-  {
-    ss << "null" << std::endl;
-  }
-  else
-    if(v.IsDouble())
-    {
-      ss << v.GetDouble() << std::endl;
-    }
-    else
-    {
-      ss << "value not exist" << std::endl;
-    }
+	if(v.IsNull())
+	{
+	  ss << "null" << std::endl;
+	}
+	else
+	  if(v.IsDouble())
+	  {
+	    ss << v.GetDouble() << std::endl;
+	  }
+	  else
+	  {
+	    ss << "value not exist" << std::endl;
+	  }
 }
 
 void dom_traverse_v2::traverse(Document &d)
@@ -328,7 +336,7 @@ void dom_traverse_v2::traverse(Document &d)
 void dom_traverse_v2::traverse_array(const Value &v,std::string path)
 {
   std::string object_key = path;
-  
+
   for (Value::ConstValueIterator itr = v.Begin(); itr != v.End(); ++itr)
   {
     const Value& array_item = *itr;
@@ -342,7 +350,7 @@ void dom_traverse_v2::traverse_array(const Value &v,std::string path)
     }
     else
     {
-  print(array_item, object_key);
+      print(array_item, object_key);
     }
   }
 }
@@ -366,17 +374,17 @@ void dom_traverse_v2::traverse_object(const Value &v,std::string path)
     else
       if (itr->value.IsArray())
       {
-  object_key.append( itr->name.GetString() );
-  object_key.append("/");
-  traverse_array(v_itr,object_key);
+	object_key.append( itr->name.GetString() );
+	object_key.append("/");
+	traverse_array(v_itr,object_key);
       }
       else
       {
-  std::string tmp = object_key;
-  object_key.append( itr->name.GetString() );
-  object_key.append("/");
-  print(v_itr, object_key);
-  object_key = tmp;
+	std::string tmp = object_key;
+	object_key.append( itr->name.GetString() );
+	object_key.append("/");
+	print(v_itr, object_key);
+	object_key = tmp;
       }
 
   }
@@ -459,8 +467,9 @@ int main(int argc, char* argv[])
 
   testing::InitGoogleTest(&argc, argv);
 
-  return RUN_ALL_TESTS();
+  //return RUN_ALL_TESTS();
 
+  std::cout << parse_json_sax( argv[1] ) ;
 }
 
-  
+
