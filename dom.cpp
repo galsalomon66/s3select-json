2ce22db40f52bf9278058e0d8edb64329dd0a348
Albin Antony <aantony@redhat.com>
2022-01-11 10:11:31 +0530

    organise

--- dom.cpp
+++ dom.cpp
@@ -1,66 +1,137 @@
 #include "rapidjson/document.h"
-#include "rapidjson/writer.h"
+#include "rapidjson/pointer.h"
 #include "rapidjson/stringbuffer.h"
+#include "dom.h"
 #include <iostream>
 #include <fstream>
-#include <chrono>
+#include <sstream>
 
 using namespace rapidjson;
 
-class Timer
+void dom_traverse_v2::print(const Value &v, std::string key_name)
 {
-	using clock_type = std::chrono::steady_clock;
-	using second_type = std::chrono::duration<double, std::ratio<1> >;
+  ss << key_name << ": ";
 
-	std::chrono::time_point<clock_type> m_beg;
-
-public:
-	Timer() : m_beg { clock_type::now() }
-	{
-	}
-
-	void reset()
-	{
-		m_beg = clock_type::now();
-	}
-
-	double elapsed() const
-	{
-		return std::chrono::duration_cast<second_type>(clock_type::now() - m_beg).count();
-	}
-};
-
-int main()
-{
-
-Timer t;
-std::string stringFromStream;
-std::ifstream in;
-in.open("generated.json", std::ifstream::in);
-if (in.is_open()) {
-    std::string line;
-    while (getline(in, line)) {
-        stringFromStream.append(line + "\n");
+  if(v.IsString())
+  {
+    ss << v.GetString() << std::endl;
+  }
+  else
+    if(v.IsInt())
+    {
+      ss << v.GetInt() << std::endl;
     }
-    in.close();
+    else
+      if(v.IsBool())
+      {
+  ss << (v.GetBool() ? "true" : "false" ) << std::endl;
+      }
+      else
+  if(v.IsNull())
+  {
+    ss << "null" << std::endl;
+  }
+  else
+    if(v.IsDouble())
+    {
+      ss << v.GetDouble() << std::endl;
+    }
+    else
+    {
+      ss << "value not exist" << std::endl;
+    }
 }
 
-Document d;
-d.Parse<0>(stringFromStream.c_str());
-//d.ParseInsitu((char*)stringFromStream.c_str());
+void dom_traverse_v2::traverse(Document &d)
+{
+  std::string path="";
 
-if (d.HasParseError()) {
-	std::cout<<"parsing error"<< std::endl;
+  for (Value::ConstMemberIterator itr = d.MemberBegin(); itr != d.MemberEnd(); ++itr)
+  {
+    const Value &v = itr->value;
+
+    if(v.IsArray())
+    {
+      std::string path="";
+      path.append( itr->name.GetString() );
+      path.append( "/" );
+
+      traverse_array(v, path);
+    }
+    else if (v.IsObject())
+    {
+      std::string path="";
+      path.append( itr->name.GetString() );
+      path.append( "/" );
+
+      traverse_object(v, path);
+    }
+    else
+    {
+      std::string tmp = path;
+      path.append( itr->name.GetString() );
+      path.append( "/" );
+      print(v, path);
+      path = tmp;
+    }
+
+  }
 }
 
-//assert(d["hello"].IsString()); 
-
-StringBuffer buffer;
-Writer<StringBuffer> writer(buffer);
-d.Accept(writer);
-
-std::cout<<"Time taken: " << t.elapsed() << " seconds\n";
- 
-std::cout << buffer.GetString() << std::endl;
-return 0;
+void dom_traverse_v2::traverse_array(const Value &v,std::string path)
+{
+  std::string object_key = path;
+  
+  for (Value::ConstValueIterator itr = v.Begin(); itr != v.End(); ++itr)
+  {
+    const Value& array_item = *itr;
+    if(array_item.IsArray())
+    {
+      traverse_array(array_item,object_key);
+    }
+    else if(array_item.IsObject())
+    {
+      traverse_object(array_item,object_key);
+    }
+    else
+    {
+  print(array_item, object_key);
+    }
+  }
 }
+
+void dom_traverse_v2::traverse_object(const Value &v,std::string path)
+{
+  std::string object_key = path;
+
+  for (Value::ConstMemberIterator itr = v.MemberBegin(); itr != v.MemberEnd(); ++itr)
+  {
+    const Value& v_itr = itr->value;
+
+    if (itr->value.IsObject())
+    {
+      std::string tmp = object_key;
+      object_key.append( itr->name.GetString() );
+      object_key.append("/");
+      traverse_object(v_itr,object_key);
+      object_key = tmp;
+    }
+    else
+      if (itr->value.IsArray())
+      {
+  object_key.append( itr->name.GetString() );
+  object_key.append("/");
+  traverse_array(v_itr,object_key);
+      }
+      else
+      {
+  std::string tmp = object_key;
+  object_key.append( itr->name.GetString() );
+  object_key.append("/");
+  print(v_itr, object_key);
+  object_key = tmp;
+      }
+
+  }
+}
+
