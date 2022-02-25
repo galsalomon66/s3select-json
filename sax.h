7bb061cad7753cbad9e0a4d05867da384bd27882
gal salomon <gal.salomon@gmail.com>
2022-02-25 18:11:32 +0200

    round 1. remove redundant variables and methods
    Signed-off-by: gal salomon <gal.salomon@gmail.com>

--- sax.h
+++ sax.h
@@ -13,251 +13,251 @@
 #include <unordered_map>
 
 class Valuesax {
-public:
+
+  public:
+
     enum Type {
-        Decimal,
-        Double,
-        String,
-        Bool,
-        Null
+      Decimal,
+      Double,
+      String,
+      Bool,
+      Null
     };
 
-    Valuesax Parse(std::string const& s) {
-    	Valuesax result;
-    	result._type = Valuesax::String;
+  private:
 
-    	result._string = s;
-    	return result;
-	  }
-
-	  Valuesax Parse(const double& s) {
-    	Valuesax result;
-    	result._type = Valuesax::Double;
-
-    	result._double = s;
-    	return result;
-	  }
-
-	  Valuesax Parse(bool& s) {
-    	Valuesax result;
-    	result._type = Valuesax::Bool;
-
-    	result._bool = s;
-    	return result;
-	  }
-
-    Valuesax Parse(const int& s) {
-    	Valuesax result;
-    	result._type = Valuesax::Decimal;
-
-    	result._num = s;
-    	return result;
-	  }
-
-	  Valuesax Parse(const unsigned& s) {
-    	Valuesax result;
-    	result._type = Valuesax::Decimal;
-
-    	result._num = s;
-    	return result;
-    }
-
-    Valuesax Parse(const int64_t& s) {
-    	Valuesax result;
-    	result._type = Valuesax::Decimal;
-
-    	result._num = s;
-    	return result;
-	  }
-
-    Valuesax Parse(const uint64_t& s) {
-    	Valuesax result;
-    	result._type = Valuesax::Decimal;
-
-    	result._num = s;
-    	return result;
-	  }
-
-	  Valuesax Parse(const std::nullptr_t& s) {
-    	Valuesax result;
-    	result._type = Valuesax::Null;
-
-    	result._null = s;
-    	return result;
-	  }
-
-    Valuesax(): _type(Decimal), _num(0), _double(0.0), _bool(false) {}
-
-    Type type() const { return _type; }
-
-    int asInt() const {
-        assert(_type == Decimal && "not an int");
-        return _num;
-    }
-
-    double asDouble() const {
-        assert(_type == Double && "not a double");
-        return _double;
-    }
-
-    std::string const& asString() const {
-        assert(_type == String && "not a string");
-        return _string;
-    }
-
-    bool asBool() const {
-        assert(_type == Bool && "not a bool");
-        return _bool;
-    }
-
-private:
     Type _type;
     double _double;
     std::string _string;
     bool _bool;
     int64_t _num;;
     std::nullptr_t _null;
+
+  public:
+
+    Valuesax(): _type(Decimal), _double(0.0), _bool(false),_num(0) {}
+
+    Valuesax Parse(std::string const& s) {
+      Valuesax result;
+      result._type = Valuesax::String;
+
+      result._string = s;
+      return result;
+    }
+
+    Valuesax Parse(const double& s) {
+      Valuesax result;
+      result._type = Valuesax::Double;
+
+      result._double = s;
+      return result;
+    }
+
+    Valuesax Parse(bool& s) {
+      Valuesax result;
+      result._type = Valuesax::Bool;
+
+      result._bool = s;
+      return result;
+    }
+
+    Valuesax Parse(const int& s) {
+      Valuesax result;
+      result._type = Valuesax::Decimal;
+
+      result._num = s;
+      return result;
+    }
+
+    Valuesax Parse(const unsigned& s) {
+      Valuesax result;
+      result._type = Valuesax::Decimal;
+
+      result._num = s;
+      return result;
+    }
+
+    Valuesax Parse(const int64_t& s) {
+      Valuesax result;
+      result._type = Valuesax::Decimal;
+
+      result._num = s;
+      return result;
+    }
+
+    Valuesax Parse(const uint64_t& s) {
+      Valuesax result;
+      result._type = Valuesax::Decimal;
+
+      result._num = s;
+      return result;
+    }
+
+    Valuesax Parse(const std::nullptr_t& s) {
+      Valuesax result;
+      result._type = Valuesax::Null;
+
+      result._null = s;
+      return result;
+    }
+
+
+    Type type() const { return _type; }
+
+    int asInt() const {
+      assert(_type == Decimal && "not an int");
+      return _num;
+    }
+
+    double asDouble() const {
+      assert(_type == Double && "not a double");
+      return _double;
+    }
+
+    std::string const& asString() const {
+      assert(_type == String && "not a string");
+      return _string;
+    }
+
+    bool asBool() const {
+      assert(_type == Bool && "not a bool");
+      return _bool;
+    }
+
 };
 
 class MyHandler : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, MyHandler> {
-    std::vector < std::pair < std::string, Valuesax>> mymap;
-    Valuesax value;
-    std::vector <Valuesax> myvalue;
-    std::string my_prev_key{};
-    std::string search_key;
-    std::string search_prev_key{};
-    std::vector <char> vect;
-    std::string keyname{};
-    std::string keyvalue;
-    std::string prev_key{};
-    bool valuep{};
-    int start_counter{};
-    std::vector<std::string> mystack;
-    std::unordered_map<int, std::string> key_stack;
-    public:
-    MyHandler() : valuep{false}, start_counter{0} {}
-    void set_search_key(std::string key) {
-      search_key = key;
-    }
-    void set_search_prev_key(std::string key) {
-      search_prev_key = key;
-    }
-    std::string get_my_prev_key()
-    {
-      return my_prev_key;
-    }
-    std::vector<Valuesax> get_myvalue() {
-      return myvalue;
-    }
-    std::vector < std::pair < std::string, Valuesax>> get_mykeyvalue() {
-      return mymap;
-    }
-    bool Null() {
-      mymap.push_back(std::make_pair(keyvalue, value.Parse(nullptr)));
-      if (keyvalue == search_key) {
-          myvalue.push_back(value.Parse(nullptr));
-        }
-      valuep = true; 
-      return true; }
-    bool Bool(bool b) {
-      mymap.push_back(std::make_pair(keyvalue, value.Parse(b)));
-      if (keyvalue == search_key) {
-          myvalue.push_back(value.Parse(b));
-        }
-      valuep = true;
-      return true; }
-    bool Int(int i) { 
-      mymap.push_back(std::make_pair(keyvalue, value.Parse(i)));
-      if (keyvalue == search_key) {
-          myvalue.push_back(value.Parse(i));
-        }
-      return true; }
-    bool Uint(unsigned u) {
-      mymap.push_back(std::make_pair(keyvalue, value.Parse(u)));
-      if (keyvalue == search_key) {
-          myvalue.push_back(value.Parse(u));
-        }
-      valuep = true;
-      return true; }
-    bool Int64(int64_t i) { 
-      mymap.push_back(std::make_pair(keyvalue, value.Parse(i)));
-      if (keyvalue == search_key) {
-          myvalue.push_back(value.Parse(i));
-        }
-      return true; }
-    bool Uint64(uint64_t u) { 
-      mymap.push_back(std::make_pair(keyvalue, value.Parse(u)));
-      if (keyvalue == search_key) {
-          myvalue.push_back(value.Parse(u));
-        }
-      return true; }
-    bool Double(double d) { 
-      mymap.push_back(std::make_pair(keyvalue, value.Parse(d)));
-      if (keyvalue == search_key) {
-          myvalue.push_back(value.Parse(d));
-        }
-      valuep = true;
-      return true; }
-    bool String(const char* str, rapidjson::SizeType length, bool copy) {
-      mymap.push_back(std::make_pair(keyvalue, value.Parse(str)));
-      if (keyvalue == search_key) {
-          myvalue.push_back(value.Parse(str));
-        }
-        valuep = true;
-        return true;
-    }
-    
+  std::vector < std::pair < std::string, Valuesax>> mymap;
+  Valuesax value;
+  //std::vector <Valuesax> myvalue;
+  //std::string my_prev_key{};
+  //std::string search_key;
+  //std::string search_prev_key{};
+  std::vector <char> vect;
+  std::string keyname{};
+  std::string keyvalue;
+  //std::string prev_key{};
+  bool valuep{};
+  size_t start_counter{};
+  std::vector<std::string> mystack;
+  std::unordered_map<int, std::string> key_stack;
+  public:
+  MyHandler() : valuep{false}, start_counter{0} {}
+  //void set_search_key(std::string key) {
+  //  search_key = key;
+  //}
+  //void set_search_prev_key(std::string key) {
+  //  search_prev_key = key;
+  //}
+  //std::string get_my_prev_key()
+  //{
+  //  return my_prev_key;
+  //}
+
+  //std::vector<Valuesax> get_myvalue() {
+  //  return myvalue;
+  //}
+
+  std::string createKey()
+  {
+      //loop on key stack
+      //pop last element
+  }
+
+  std::vector < std::pair < std::string, Valuesax>> get_mykeyvalue() {
+    return mymap;
+  }
+
+  bool Null() {
+    mymap.push_back(std::make_pair(keyvalue, value.Parse(nullptr)));
+    valuep = true; 
+    return true; }
+
+  bool Bool(bool b) {
+    mymap.push_back(std::make_pair(keyvalue, value.Parse(b)));
+    valuep = true;
+    return true; }
+
+  bool Int(int i) { 
+    mymap.push_back(std::make_pair(keyvalue, value.Parse(i)));
+    return true; }
+
+  bool Uint(unsigned u) {
+    mymap.push_back(std::make_pair(keyvalue, value.Parse(u)));
+    valuep = true;
+    return true; }
+
+  bool Int64(int64_t i) { 
+    mymap.push_back(std::make_pair(keyvalue, value.Parse(i)));
+    return true; }
+
+  bool Uint64(uint64_t u) { 
+    mymap.push_back(std::make_pair(keyvalue, value.Parse(u)));
+    return true; }
+
+  bool Double(double d) { 
+    mymap.push_back(std::make_pair(keyvalue, value.Parse(d)));
+    valuep = true;
+    return true; }
+
+  bool String(const char* str, rapidjson::SizeType length, bool copy) {
+    mymap.push_back(std::make_pair(keyvalue, value.Parse(str)));
+    valuep = true;
+    return true;
+  }
+
   bool StartObject() {
     if(!valuep) {
-      if (mystack.size() == 0 || mystack[mystack.size() - 1] != keyname) {
-        if (keyname.length()) {
-        mystack.push_back(keyname);
-        start_counter = vect.size();
-        key_stack[start_counter] = keyname;
-      }
+      //if (mystack.size() == 0 || mystack[mystack.size() - 1] != keyname) {
+      if (mystack.size() == 0 || mystack.front() != keyname) {
+	if (keyname.length()) {
+	  mystack.push_back(keyname);
+	  start_counter = vect.size();
+	  key_stack[start_counter] = keyname;
+	}
       }
     }
     vect.push_back('{');
     return true; 
-    }
+  }
 
   bool Key(const char* str, rapidjson::SizeType length, bool copy) {
-    prev_key = keyvalue;
+    //prev_key = keyvalue;
     std::vector<std::string> stack{mystack};
     stack.push_back(str);
     valuep = false;
     keyvalue = "";
-    
+
     for (const auto& i: stack) {
       if (i != "") {
-      keyvalue += i + '/';
+	keyvalue += i + '/';
       }
     }
     keyname = str;
-    if (search_prev_key == prev_key) {
-      my_prev_key = keyvalue;
-    }
+   // if (search_prev_key == prev_key) {
+   //   my_prev_key = keyvalue;
+   // }
     return true;
-    }
+  }
 
   bool EndObject(rapidjson::SizeType memberCount) {
     vect.pop_back();
     if (mystack.size() > 0 && vect.size()  == start_counter) {
-    if(key_stack[vect.size()] == mystack.back()) {
-    mystack.pop_back();
-  }
+      if(key_stack[vect.size()] == mystack.back()) {
+	mystack.pop_back();
+      }
 
-  --start_counter;
-  }
-    return true;
+      --start_counter;
     }
+    return true;
+  }
 
   bool StartArray() {
     if(!valuep) {
       if (mystack.size() == 0 || mystack[mystack.size() - 1] != keyname) {
-        mystack.push_back(keyname);
-        start_counter = vect.size();
-        key_stack[start_counter] = keyname;
+	mystack.push_back(keyname);
+	start_counter = vect.size();
+	key_stack[start_counter] = keyname;
       }
     }
     vect.push_back('[');
@@ -268,10 +268,10 @@ class MyHandler : public rapidjson::BaseReaderHandler<rapidjson::UTF8<>, MyHandl
     vect.pop_back();
     if (mystack.size() > 0 && vect.size()  == start_counter) {
       if(key_stack[vect.size()] == mystack.back()) {
-        mystack.pop_back();
+	mystack.pop_back();
       }
-    --start_counter;
-  }
+      --start_counter;
+    }
     return true; 
   }
 };
--- test_json.cpp
+++ test_json.cpp
@@ -59,15 +59,14 @@ std::string parse_json_dom(const char* file_name)
   return final_result;
 }
 
-std::string parse_json_sax(const char* buff, const char* key, size_t size)
+std::string extract_key_values(const char* buff,uint64_t buffer_sz)
 {
   std::stringstream result;
   std::string final_result;
 
-  rapidjson::MemoryStream buffer(buff, size);
+  rapidjson::MemoryStream buffer(buff, buffer_sz);
 
   MyHandler handler;
-  handler.set_search_key(key);
   rapidjson::Reader reader{};
 
   reader.IterativeParseInit();
@@ -82,227 +81,42 @@ std::string parse_json_sax(const char* buff, const char* key, size_t size)
     }
   
   for (const auto& i : handler.get_mykeyvalue()) {
-    result<<i.first<<": ";
-    switch((i.second).type()) {
-      case Valuesax::Decimal: result << (i.second).asInt() << "\n"; break;
-      case Valuesax::Double: result << (i.second).asDouble() << "\n"; break;
-      case Valuesax::String: result << (i.second).asString() << "\n"; break;
-      case Valuesax::Bool: result << std::boolalpha << (i.second).asBool() << "\n"; break;
-      case Valuesax::Null: result << "null" << "\n"; break;
+    switch(i.second.type()) {
+      case Valuesax::Decimal: result << i.first << " : " << i.second.asInt() << "\n"; break;
+      case Valuesax::Double: result << i.first << " : " << i.second.asDouble() << "\n"; break;
+      case Valuesax::String: result << i.first << " : " << i.second.asString() << "\n"; break;
+      case Valuesax::Bool: result << i.first << " : " << std::boolalpha << i.second.asBool() << "\n"; break;
+      case Valuesax::Null: result << i.first << " : " << "null" << "\n"; break;
       default: break;
     }
   }
   final_result = result.str();
   return final_result;
 }
-
-std::string get_value_sax(const char* buff, const char* key, size_t size)
-{
-  std::stringstream result;
-  std::string final_result;
-
-  rapidjson::MemoryStream buffer(buff, size);
-
-  MyHandler handler;
-  handler.set_search_key(key);
-  rapidjson::Reader reader{};
-
-  reader.IterativeParseInit();
-  while (!reader.IterativeParseComplete()) {
-    reader.IterativeParseNext<rapidjson::kParseDefaultFlags>(buffer, handler);
-    if(reader.HasParseError())  {
-      rapidjson::ParseErrorCode c = reader.GetParseErrorCode();
-      size_t o = reader.GetErrorOffset();
-      std::cout << "PARSE ERROR " << c << " " << o << std::endl;
-      break;
-      }
-    }
-  
-  for (const auto& i : handler.get_myvalue()) {
-    switch(i.type()) {
-      case Valuesax::Decimal: result << i.asInt() << "\n"; break;
-      case Valuesax::Double: result << i.asDouble() << "\n"; break;
-      case Valuesax::String: result << i.asString() << "\n"; break;
-      case Valuesax::Bool: result << std::boolalpha << i.asBool() << "\n"; break;
-      case Valuesax::Null: result << "null" << "\n"; break;
-      default: break;
-    }
-  }
-  final_result = result.str();
-  return final_result;
-}
-
-std::string get_next_key_sax(const char* buff, const char* key, size_t size)
-{
-  std::stringstream result;
-  std::string final_result;
-  
-  rapidjson::MemoryStream buffer(buff, size);
-
-  MyHandler handler;
-  handler.set_search_prev_key(key);
-  rapidjson::Reader reader{};
-
-  reader.IterativeParseInit();
-  while (!reader.IterativeParseComplete()) {
-    reader.IterativeParseNext<rapidjson::kParseDefaultFlags>(buffer, handler);
-    if(reader.HasParseError())  {
-      rapidjson::ParseErrorCode c = reader.GetParseErrorCode();
-      size_t o = reader.GetErrorOffset();
-      std::cout << "PARSE ERROR " << c << " " << o << std::endl;
-      break;
-      }
-    }
-  
-  final_result = handler.get_my_prev_key();
-  return final_result;
-}
-/*
-TEST(Jsonparse, json)
-{
-  
-
-  //std::string dom_result = parse_json_dom("sample4.json");
-
-  //ASSERT_EQ(dom_result, sax_result);
-}
-
-TEST(Jsonparse, json1)
-{
-  std::string sax_result_1 = parse_json_sax("sample2.json");
-
-  std::string dom_result_1 = parse_json_dom("sample2.json");
-
-  ASSERT_EQ(dom_result_1, sax_result_1);
-}
-
-TEST(Jsonparse, json2)
-{
-  std::string sax_result_2 = parse_json_sax("sample3.json");
-
-  std::string dom_result_2 = parse_json_dom("sample3.json");
-
-  ASSERT_EQ(dom_result_2, sax_result_2);
-}
-
-TEST(Jsonparse, json3)
-{
-  std::string sax_result_3 = parse_json_sax("sample8.json");
-
-  std::string dom_result_3 = parse_json_dom("sample8.json");
-
-  ASSERT_EQ(dom_result_3, sax_result_3);
-}
-
-TEST(Jsonparse, json4)
-{
-  std::string sax_result_4 = parse_json_sax("sample9.json");
-
-  std::string dom_result_4 = parse_json_dom("sample9.json");
-
-  ASSERT_EQ(dom_result_4, sax_result_4);
-}
-
-TEST(Jsonparse, json5)
-{
-  std::string sax_result_5 = parse_json_sax("sample13.json");
-
-  std::string dom_result_5 = parse_json_dom("sample13.json");
-
-  ASSERT_EQ(dom_result_5, sax_result_5);
-}*/
 
 int main(int argc, char* argv[])
-{/*
-  testing::InitGoogleTest(&argc, argv);
-
-  return RUN_ALL_TESTS();*/
-
+{
   std::ifstream input_file_stream;
   size_t size;
-  size_t m_processed_bytes;
 
   char* file_name = argv[1];
 
   try {
     input_file_stream = std::ifstream(file_name, std::ios::in | std::ios::binary);
   }
-  catch( ... )  {
-  std::cout << "failed to open file " << std::endl;  
-  exit(-1);
+  catch( ... ){
+    std::cout << "failed to open file " << std::endl;  
+    exit(-1);
   }
-
-  constexpr double buffer_size {100};
-  std::string m_last_line;
-  bool m_previous_line;
-  std::string merge_line;
-  size_t merge_size;
-  char* buff = (char*)malloc(buffer_size);
-
-  const char* key = "Source Data/key=4lwicsuorhkvkoqzlgfcig/";
 
   auto file_sz = std::filesystem::file_size(file_name);
+  char* buff = (char*)malloc(file_sz);
 
-  std::vector <char> stack;
 
-  while(1) {
-  size = input_file_stream.readsome(buff, buffer_size);
-  std::cout << "@@read from file" << std::endl;
+  size = input_file_stream.readsome(buff, file_sz);
+  //std::cout << "@@read from file" << std::endl;
 
-  if(!size || input_file_stream.eof()){
-      break;
-  }
+  std::cout << extract_key_values(buff, file_sz) << std::endl;
 
-  std::string tmp_buff;
-
-  char* p_obj_chunk = (char*)buff;
-  char* prev_chunk = (char*)buff;
-  size_t start_index{};
-  size_t len{};
-  while (p_obj_chunk < buff + size) {
-    switch(*p_obj_chunk)
-    {
-      case '{':
-      case '[':
-      case '(':
-        stack.push_back('(');
-        break;
-      case '}':
-      case ']':
-      case ')':
-        stack.pop_back();
-        break;
-      default:
-        break;  
-    }
-    p_obj_chunk++;
-    len = p_obj_chunk - prev_chunk;
-    if(stack.empty()) {
-      std::cout << "@@empty stack" << std::endl;
-      tmp_buff.assign((char*)buff, start_index, len);
-      merge_line += tmp_buff;
-      merge_size = merge_line.size();
-      std::string sax_result = get_value_sax(merge_line.c_str(), key, merge_size);
-      std::cout<<sax_result;
-      std::string sax_next_key = get_next_key_sax(merge_line.c_str(), key, merge_size);
-      std::cout<<sax_next_key<<"\n";
-      std::string sax_result_4 = parse_json_sax(merge_line.c_str(), key, merge_size);
-      std::cout << "@@ merge line size = " << merge_line.size() << std::endl;
-      std::cout<<sax_result_4;
-      if (tmp_buff.size() + 1 == size || merge_size + 2 >= file_sz) {
-        break;
-      } else {
-        start_index = p_obj_chunk - buff;
-        prev_chunk = p_obj_chunk;
-      }
-    }
-  }
-  
-  if (tmp_buff.size() + 1 == size) {
-    continue;
-  } else {
-    tmp_buff.assign((char*)buff, start_index, len);
-    merge_line += tmp_buff;
-  }
-  }
+  return 0;
 }
